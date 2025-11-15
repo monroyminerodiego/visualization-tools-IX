f8c9ca889430ae66a231d1b5a640d6a0fafe933f
monroyminerodiego <diego.monroy.minero@outlook.com>
2025-11-15 17:05:13 -0600

    v 1.4.1: Hotfixes para que funcionen rutas

--- Web/static/js/index.js
+++ Web/static/js/index.js
@@ -162,7 +162,7 @@ function navigateToProject(unitNumber) {
     }
     
     // Construct the correct URL using the unit number
-    const url = `/visualization-tools/project/unit${unitNumber}`;
+    const url = `/project/unit${unitNumber}`;
     
     console.log(`Navigating to project unit ${unitNumber}:`, url);
     
@@ -194,7 +194,7 @@ function navigateToPortfolio(unitNumber) {
     }
     
     // Construct the correct URL using the unit number
-    const url = `/visualization-tools/portfolio/unit${unitNumber}`;
+    const url = `/portfolio/unit${unitNumber}`;
     
     console.log(`Navigating to portfolio unit ${unitNumber}:`, url);
     
